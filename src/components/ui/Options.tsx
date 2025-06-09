dce84558c530395c265ee2418651b6db8b29d46c
Luis Pinto <dapin46@hotmail.com>
2025-06-08 22:05:23 -0500

    Updated: Options component modified to no render undefined defaultOption

--- src/components/ui/Options.tsx
+++ src/components/ui/Options.tsx
@@ -28,7 +28,7 @@ export const Options = ({
         name={name}
         className="border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring mt-2 flex h-10 w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
       >
-        <option value="">{defaultOption}</option>
+        {defaultOption && <option value="">{defaultOption}</option>}
         {children}
       </select>
     </div>
