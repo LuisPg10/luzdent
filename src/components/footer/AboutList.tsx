d263b4104ffbcbbefb8c409506c87cfd55f6186a
Luis Pinto <dapin46@hotmail.com>
2025-08-06 22:05:27 -0500

    Update: Footer updated to show team in about us

--- src/components/footer/AboutList.tsx
+++ src/components/footer/AboutList.tsx
@@ -4,7 +4,7 @@ export const AboutList = () => {
       <h3 className="text-lg font-medium">Empresa</h3>
       <ul className="space-y-2">
         <li>
-          <a href="#nosotros" className="text-gray-200 hover:text-white">
+          <a href="#equipo" className="text-gray-200 hover:text-white">
             Sobre nosotros
           </a>
         </li>
